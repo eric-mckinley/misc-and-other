f18a3514255ee86d2786026ea3651190425be735
Eric McKinley <mckinley.eric@yahoo.co.uk>
2019-01-17 08:51:36 +0000

    store change

--- one-vs-one/js/store.js
+++ one-vs-one/js/store.js
@@ -1,28 +1,25 @@
 
 var storeURL = 'https://api.myjson.com/bins'
 
-{
-    "uri": "https://api.myjson.com/bins/jzbes"
-}
-
 function createStore(data, callbackSuccess) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', storeURL, true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    xhr.responseType = 'json';
+//    xhr.responseType = 'json';
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 201) {
-            console.log("done, " + xhr.response);
+            var created = JSON.parse(xhr.responseText);
+            var n = created.uri.lastIndexOf("/");
+            var binId = created.uri.substr(n +1);
+            callbackSuccess(binId)
         }
     }
     xhr.send(JSON.stringify(data));
-
 };
 
 
-function updateStore(id, data, callbackSuccess) {
+function addScore(binId, scoreA, scoreB) {
 }
 
 function getStore(id, callbackSuccess) {
